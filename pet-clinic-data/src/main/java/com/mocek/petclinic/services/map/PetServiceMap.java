da3461d713d9eb7b401420275550441811e5e4b3
mmocek <michal.mocek@nokia.com>
2018-08-27 17:44:40 +0200

    Added Pets and Contact info to owners on startup.

--- pet-clinic-data/src/main/java/com/mocek/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/mocek/petclinic/services/map/PetServiceMap.java
@@ -2,10 +2,11 @@ package com.mocek.petclinic.services.map;
 
 import com.mocek.petclinic.model.Pet;
 import com.mocek.petclinic.services.CrudService;
+import com.mocek.petclinic.services.PetService;
+import com.mocek.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
-
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
 }
